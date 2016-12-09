6ace26fcd53ae4d2fc18598639e8895dcedd7f62
amrita <amrita@cloudlendinginc.com>
2016-12-09 23:09:23 +0530

    JIRA ID : Compiler DESCRIPTION : TEST RUN : COMMENTS : None

--- APEXC.py
+++ APEXC.py
@@ -30,17 +30,18 @@ def main(argv):
                 folder = arg
 
 
-        workingDir = "C:\\Users\\Amrita\\PycharmProjects\\workingDir\\"
+        workingDir = os.path.join(folder, 'workingDir')
+    #"C:\\Users\\Amrita\\PycharmProjects\\workingDir\\"
 
 #check if workingDir is already present , delete it
 
         if(os.path.isdir(workingDir) == True):
             shutil.rmtree(workingDir)
 
-        shutil.copytree(folder+'\\classes', workingDir)
+        shutil.copytree(os.path.join(folder, 'classes'), workingDir)
 
     # call object to java converter
-        objDir = folder + '\\objects'
+        objDir = os.path.join(folder, 'objects')
         print('Object dir :' + objDir);
         createPoJoForSFObject(objDir, workingDir)
 
@@ -48,34 +49,18 @@ def main(argv):
 		extension = path.splitext(filename)[1]
 		if extension == '.cls':
                     newfile = filename.replace(extension, '.java')
-                    os.rename(workingDir+'\\'+filename, workingDir+'\\'+newfile)
+                    os.rename(os.path.join(workingDir, filename), os.path.join(workingDir, newfile))
 
 
         # call Modify System Lib to MfiflexSystem Lib
         for filename in listdir(workingDir):
             modify_System_Lib(filename, workingDir)
 
-        file1 = "MfiflexSystem.java"
-        file2 = "LoggingLevel.java"
-        file3 = "Date.java"
-        file4 = "ID.java"
-        file5 = "DateTime.java"
-        file6 = "sObject.java"
-        file7 = "Database.java"
-        file8 = "Savepoint.java"
-        file9 = "DMLException.java"
-        file10 = "mfiflexUtil.java"
+        libFolder = os.path.join(folder, 'lib');
+        for file in listdir(libFolder):
+            shutil.copy(os.path.join(libFolder, file), os.path.join(workingDir, file))
+        #shutil.copytree(libFolder, workingDir)
 
-        shutil.copy('C:\\Users\\Amrita\\PycharmProjects\\APEXC\\src\\'+file1, workingDir+'\\'+file1)
-        shutil.copy('C:\\Users\\Amrita\\PycharmProjects\\APEXC\\src\\'+file2, workingDir + '\\'+file2)
-        shutil.copy('C:\\Users\\Amrita\\PycharmProjects\\APEXC\\src\\' + file3, workingDir + '\\' + file3)
-        shutil.copy('C:\\Users\\Amrita\\PycharmProjects\\APEXC\\src\\' + file4, workingDir + '\\' + file4)
-        shutil.copy('C:\\Users\\Amrita\\PycharmProjects\\APEXC\\src\\' + file5, workingDir + '\\' + file5)
-        shutil.copy('C:\\Users\\Amrita\\PycharmProjects\\APEXC\\src\\' + file6, workingDir + '\\' + file6)
-        shutil.copy('C:\\Users\\Amrita\\PycharmProjects\\APEXC\\src\\' + file7, workingDir + '\\' + file7)
-        shutil.copy('C:\\Users\\Amrita\\PycharmProjects\\APEXC\\src\\' + file8, workingDir + '\\' + file8)
-        shutil.copy('C:\\Users\\Amrita\\PycharmProjects\\APEXC\\src\\' + file9, workingDir + '\\' + file9)
-        shutil.copy('C:\\Users\\Amrita\\PycharmProjects\\APEXC\\src\\' + file10, workingDir + '\\' + file10)
     # call javac compiler
 	cmd = "javac "+workingDir+"\*.java"
 	proc = subprocess.Popen(cmd, shell=True)
