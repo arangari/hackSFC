380b22863309bc653679a7781f517080ffce6fbb
Amit H Rangari <amit.rangari@cloudlendinginc.com>
2016-12-09 14:28:02 +0530

    added the createPoJoForSFObject with loop

--- SFObjectParser.py
+++ SFObjectParser.py
@@ -2,17 +2,9 @@
 # Simple xml parser for salesforce object file .object
 
 import xml.etree.ElementTree as ET
-
-tree = ET.parse('Loan_Account__c.object')
-root = tree.getroot()
-
-#for child in root:
-#    print "tag = " , child.tag
-#    print "attrib = ", child.attrib
-
-ns = {'customObject' : 'http://soap.sforce.com/2006/04/metadata',
-
-     }
+import sys
+import os
+from os.path import isfile, join
 
 class CustomField:
     def __init__(self, name):
@@ -72,15 +64,14 @@ class CustomField:
         #    print 'Picklist not supported yet'
         #    return ""
 
-
-
-
-
 class SFObject:
-    Name = 'some-name'
-    fields = []
+
+    count = 0
+
     def __init__(self, name):
         self.Name = name
+        self.fields = []
+        SFObject.count  += 1
 
     def addField(self, cfield):
         self.fields.append(cfield)
@@ -91,13 +82,19 @@ class SFObject:
             cf.display()
 
     #print the object class
-    def dumpJavaClass(self) :
+    def dumpJavaClass(self, dir) :
         ocName = self.Name + '.cls'
-        oc = open(ocName, "w")
+        ocPath = dir + '/' + ocName
+        print 'ocPath = ', ocPath
+        oc = open(ocPath, "w")
 
         oc.write("//This is automated geneerated file!! \n\n")
 
-        classString = "public class " + self.Name + "\t extends SalesForceObject {"
+        #this is added to handle date declaration
+        oc.write("\nimport java.util.Date;")
+        oc.write("\n")
+
+        classString = "\npublic class " + self.Name + "\t extends SalesForceObject {"
 
 
 
@@ -108,46 +105,80 @@ class SFObject:
         oc.write("\n\n")
         for cf in self.fields :
             memberString = cf.dumpJavaMember()
-            print 'memberstring = :',memberString,':'
+            #print 'memberstring = :',memberString,':'
             oc.write(memberString)
             oc.write("\n\n")
 
         oc.write("}")
         oc.close()
 
-loanAccount = SFObject('Loan_Account__c')
+
+def getParsedObject(oroot, objectName) :
+
+    object = SFObject(objectName)
+
+    ns = {'customObject' : 'http://soap.sforce.com/2006/04/metadata',
+
+         }
 
 
-for fields in root.findall('customObject:fields', ns):
-    #get the full name
-    fullName = fields.find('customObject:fullName', ns).text
-    cfield = CustomField(fullName)
 
-    #get the description
-    #description = fields.find('customObject:description', ns).text
-    #cfield.setDescription(description)
+    for fields in oroot.findall('customObject:fields', ns):
+        #get the full name
+        fullName = fields.find('customObject:fullName', ns).text
+        cfield = CustomField(fullName)
 
-    #get the externalId flag
-    externalId = fields.find('customObject:externalId', ns).text
+        #get the description
+        #description = fields.find('customObject:description', ns).text
+        #cfield.setDescription(description)
 
-    #get the label
-    label = fields.find('customObject:label', ns).text
+        #get the externalId flag
+        externalId = fields.find('customObject:externalId', ns).text
 
-    mytype = fields.find('customObject:type', ns).text
+        #get the label
+        label = fields.find('customObject:label', ns).text
 
-    cfield.addDetail('label', label)
-    cfield.addDetail('type', mytype)
-    cfield.addDetail('externalId', externalId)
+        mytype = fields.find('customObject:type', ns).text
 
-    #print 'custom field is :'
-    #cfield.display()
+        cfield.addDetail('label', label)
+        cfield.addDetail('type', mytype)
+        cfield.addDetail('externalId', externalId)
 
-    loanAccount.addField(cfield)
+        #print 'custom field is :'
+        #cfield.display()
+
+        object.addField(cfield)
+
+    return object
 
 
-#print 'loanAccount = '
-#loanAccount.display()
-loanAccount.dumpJavaClass()
+
+def createPoJoForSFObject(objDir, targetDir):
+
+    print 'Target directory is: ', targetDir
+    print 'Object directory is: ', objDir
+
+    files = os.listdir(objDir)
+
+    for ff in files :
+        print 'ff = ', ff
+        objectName = ff.replace('.object', '')
+        print 'objectName = ', objectName
+        fullff = objDir + '/' + ff
+        print 'fullff = ', fullff
+        tree = ET.parse(fullff)
+        root = tree.getroot()
+        object =  getParsedObject(root, objectName)
+        #object.display()
+        object.dumpJavaClass(targetDir)
+
+
+objDir = sys.argv[1]
+targetDir = sys.argv[2]
+createPoJoForSFObject(objDir, targetDir)
+
+
+
 
 
 
